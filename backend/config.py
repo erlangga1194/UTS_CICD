a352b8053485bdf2ec6d4046830d43647ae5ed32
Erlangga <anggaerlangga108@gmail.com>
2025-08-07 14:02:23 +0700

    first commit

---
+++ backend/config.py
@@ -0,0 +1,6 @@
+RDS_HOST = 'database-uts.cdseoiuy0z06.ap-southeast-2.rds.amazonaws.com'
+DB_USER = 'cobacoba'
+DB_PASS = 'erlangga123'
+DB_NAME = 'database_uts'
+
+S3_BUCKET = 'utsbucket1794'
---
+++ frontend/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+import pymysql
+import boto3
+
+app = Flask(__name__)
+
+# Backend config
+import backend.config as cfg
+
+# RDS connection
+conn = pymysql.connect(host=cfg.RDS_HOST, user=cfg.DB_USER, password=cfg.DB_PASS, db=cfg.DB_NAME)
+cur = conn.cursor()
+
+# S3 config
+s3 = boto3.client('s3')
+bucket = cfg.S3_BUCKET
+
+@app.route('/')
+def index():
+    cur.execute("SELECT name, price FROM products")
+    products = cur.fetchall()
+
+    images = s3.list_objects_v2(Bucket=bucket).get('Contents', [])
+    image_urls = [f"https://{bucket}.s3.amazonaws.com/{img['Key']}" for img in images]
+
+    return render_template('index.html', products=products, images=image_urls)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
